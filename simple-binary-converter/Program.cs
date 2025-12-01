02971610cbb54dd693efdea1c0a569bffa497fc2
Student <foobar@sr.htlweiz.at>
2025-12-01 10:24:58 +0100

    startet

---
+++ simple-binary-converter/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace simple_binary_converter 
+{
+    public class Program
+    {
+        public static int Main(string argv)
+        {
+            Int64 current = 0;
+            while (current == 0)
+            {
+                
+            }
+            return 0;
+        }
+    }
+}
